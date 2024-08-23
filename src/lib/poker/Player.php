db2aec9f0e19de371268026d589f72dbe15b0388
github yosshi-kataoka <github>
2024-08-24 06:28:33 +0900

    単一責任の原則に沿って、ポーカーカードプログラムをリファクタリングしました。

---
+++ src/lib/poker/Player.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace poker;
+
+require_once('Card.php');
+class Player
+{
+  public function __construct(private Card $cards) {}
+
+  public function getNumber()
+  {
+    return $this->cards->getNumber();
+  }
+  public function getSuit()
+  {
+    return $this->cards->getSuit();
+  }
+}
--- src/lib/poker/PokerGame.php
+++ src/lib/poker/PokerGame.php
@@ -1,11 +1,22 @@
 <?php
 
+namespace poker;
+
+require_once('Card.php');
+require_once('Player.php');
+
 class PokerGame
 {
   public function __construct(private array $cards1, private array $cards2) {}
 
   public function start(): array
   {
-    return [$this->cards1, $this->cards2];
+    $playerRanks = [];
+    foreach ([$this->cards1, $this->cards2] as $cards) {
+      $playerCard = new Card($cards);
+      $player = new Player($playerCard);
+      $playerRanks[] = $player->getNumber();
+    }
+    return $playerRanks;
   }
 }
