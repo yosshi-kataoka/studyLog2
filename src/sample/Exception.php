8ff706541600b98e3173bb14869b5c8d399ebe81
github yosshi-kataoka <github>
2024-09-02 16:18:40 +0900

    例外処理の学習として、Exception.phpを作成。

---
+++ src/sample/Exception.php
@@ -0,0 +1,31 @@
+<?php
+
+class CurrencyCal
+{
+
+  const CURRENCIES =
+  [
+    'dollar',
+    'pound'
+  ];
+
+  public static function calculateToYen($amount, $currency)
+  {
+    try {
+      self::convert($amount, $currency);
+    } catch (Exception $e) {
+      echo '例外: ' . $e->getMessage() . PHP_EOL;
+    }
+  }
+
+  private static function convert($amount, $currency)
+  {
+    if (!in_array($currency, self::CURRENCIES)) {
+      throw new Exception('対応していない通貨です');
+    }
+    echo $amount . '円を' . $currency . 'に変換します' . PHP_EOL;
+  }
+}
+
+CurrencyCal::calculateToYen(100, 'dollar');
+// CurrencyCal::calculateToYen(100, 'won');
