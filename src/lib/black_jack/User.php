b6be4df57343b4b2142ec9bea496c5879d22bbc2
github yosshi-kataoka <github>
2024-10-01 06:13:37 +0900

    プレイヤーが引いたカードをコンソール上に表示する処理を実装。

--- src/lib/black_jack/User.php
+++ src/lib/black_jack/User.php
@@ -4,5 +4,10 @@ namespace BlackJack;
 
 abstract class User
 {
-  abstract function drawCard(Deck $deck);
+  protected array $hands = [];
+  protected int $totalCardsNumber = 0;
+  protected string $name = '';
+  abstract function drawCard(Deck $deck): array;
+  abstract function getTotalCardsNumber(): int;
+  abstract function getCardMessage(): void;
 }
