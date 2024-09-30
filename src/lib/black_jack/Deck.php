35791458f890a634be11bccf0ecff7c6c6106fd4
github yosshi-kataoka <github>
2024-10-01 04:22:28 +0900

    カードのマークを'ハート','スペード','ダイヤ','クラブ'にそれぞれ変更しました。

--- src/lib/black_jack/Deck.php
+++ src/lib/black_jack/Deck.php
@@ -14,7 +14,7 @@ class Deck
   public function __construct(private Card $card = new Card())
   {
     // ジョーカーを除く52枚のカードを生成しシャッフルする
-    foreach (['H', 'S', 'D', 'C'] as $suit) {
+    foreach (['ハート', 'スペード', 'ダイヤ', 'クラブ'] as $suit) {
       foreach (['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] as $number) {
         $this->cards[] = ['suit' => $suit, 'number' => $number];
       }
